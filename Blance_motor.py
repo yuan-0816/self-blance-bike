90274f29fe3a5a48911c8ec1b8c832f94840c629
yuan-0816 <wj582693@gmail.com>
2022-12-02 23:43:26 +0800

    20221202 23:43 add blance motor

--- Blance_motor.py
+++ Blance_motor.py
@@ -6,6 +6,14 @@ BLANCE_MOTOR_PWM_FREQ = 8000
 
 pi = pigpio.pi()
 
+pi.hardware_PWM(BLANCE_MOTOR_PIN, BLANCE_MOTOR_PWM_FREQ, 900000)
+time.sleep(1)
+pi.hardware_PWM(BLANCE_MOTOR_PIN, BLANCE_MOTOR_PWM_FREQ, 800000)
+time.sleep(1)
+pi.hardware_PWM(BLANCE_MOTOR_PIN, BLANCE_MOTOR_PWM_FREQ, 700000)
+time.sleep(1)
+pi.hardware_PWM(BLANCE_MOTOR_PIN, BLANCE_MOTOR_PWM_FREQ, 600000)
+time.sleep(1)
 pi.hardware_PWM(BLANCE_MOTOR_PIN, BLANCE_MOTOR_PWM_FREQ, 500000)
-time.sleep(5)
-pi.set_mode(BLANCE_MOTOR_PIN, pigpio.INPUT)
+time.sleep(1)
+pi.set_mode(BLANCE_MOTOR_PIN, pigpio.OUTPUT)
